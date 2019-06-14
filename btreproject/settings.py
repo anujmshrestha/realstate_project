a2c88f72845babffbb89104b59b24eed703c13b3
AGStha <gongmakz.anuj@gmail.com>
2019-06-14 09:21:15 +0545

    whitenoise added

--- btreproject/settings.py
+++ btreproject/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
